a1db6dd51ec7fd2fe44b519e89a8ef298e8d2e25
Flavio Contreras <flaviohcontreras@live.com>
2016-11-19 11:28:52 -0200

    Style todo application

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,6 +5,8 @@ var Todo = React.createClass({
 
   render(){
     var {id, text, completed, createdAt, completedAt} = this.props;
+    var todoClassName = completed ? 'todo todo-completed' : 'todo';
+
     var renderDate = () => {
       var message = 'Created ';
       var timestamp = createdAt;
@@ -17,12 +19,16 @@ var Todo = React.createClass({
     };
 
     return (
-      <div onClick={() => {
+      <div className={todoClassName} onClick={() => {
           this.props.onToggle(id);
         }}>
-        <input type="checkbox" checked={completed} readOnly/>
-        <p>{text}</p>
-        <p>{renderDate()}</p>
+        <div>
+            <input type="checkbox" checked={completed} readOnly/>
+        </div>
+        <div>
+          <p>{text}</p>
+          <p className='todo__subtext'>{renderDate()}</p>  
+        </div>
       </div>
     );
   }
