64166f028366c01b9c0c9f2dc05e8c8b907692a5
Flavio Contreras <flaviohcontreras@live.com>
2016-11-19 11:03:01 -0200

    Add completed and created timestamp

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import moment from 'moment';
 
 var Todo = React.createClass({
 
   render(){
-    var {id, text, completed} = this.props;
+    var {id, text, completed, createdAt, completedAt} = this.props;
+    var renderDate = () => {
+      var message = 'Created ';
+      var timestamp = createdAt;
+      if (completed){
+        message = 'Completed ';
+        timestamp = completedAt;
+      }
+
+      return `${message}${moment.unix(timestamp).format('MMM Do Y @ HH:mm a')}`
+    };
 
     return (
       <div onClick={() => {
           this.props.onToggle(id);
         }}>
         <input type="checkbox" checked={completed} readOnly/>
-        {text}
+        <p>{text}</p>
+        <p>{renderDate()}</p>
       </div>
     );
   }
--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import uuid from 'node-uuid';
+import moment from 'moment';
 
 // components
 import TodoList from 'TodoList';
@@ -25,7 +26,9 @@ var TodoApp = React.createClass({
         {
           id: uuid(),
           text: text,
-          completed: false
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
         }
       ]
     });
@@ -40,6 +43,7 @@ var TodoApp = React.createClass({
     var updatedTodos = this.state.todos.map((todo) => {
       if (todo.id === id) {
         todo.completed = !todo.completed;
+        todo.completedAt = todo.completed ? moment().unix() : undefined;
       }
 
       return todo;
