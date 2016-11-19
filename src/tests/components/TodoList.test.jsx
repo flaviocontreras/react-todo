85f0d5ee1a0d63da2738eb489f4f0b3208c1ce5a
Flavio Contreras <flaviohcontreras@live.com>
2016-11-18 23:05:36 -0200

    Update TodoList test suite

--- src/tests/components/TodoList.test.jsx
+++ src/tests/components/TodoList.test.jsx
@@ -22,6 +22,6 @@ describe('TodoList', () => {
     var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
     var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
 
-    expect(todosComponents.length).toBe(todos.length);
+    expect(todosComponents.length).toBe(todos.length);cd
   });
 });
