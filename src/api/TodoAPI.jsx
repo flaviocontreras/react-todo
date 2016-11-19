0cb4fe1d72666348dc1a6957f681299e2a55786d
Flavio Contreras <flaviohcontreras@live.com>
2016-11-19 10:27:24 -0200

    Add filter functionality

--- src/api/TodoAPI.jsx
+++ src/api/TodoAPI.jsx
@@ -16,5 +16,32 @@ export default {
     }
 
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos(todos, showCompleted, searchText){
+    var searchText = searchText.toLowerCase();
+    var filteredTodos = todos;
+
+    // Filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+    // Filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      return todo.text.toLowerCase().indexOf(searchText) > -1;
+    });
+
+    // Sorto todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        return -1;
+      } else if ( a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+
+    });
+
+    return filteredTodos;
   }
 };
