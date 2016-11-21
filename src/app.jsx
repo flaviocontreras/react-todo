e78902bdd9e88e47b452e57b32de35c752b7441c
Flavio Contreras <flaviohcontreras@live.com>
2016-11-21 00:33:05 -0200

    Add redux store

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Route, Router, IndexRoute, browserHistory} from 'react-router';
 import TodoApp from 'TodoApp';
+
+import * as actions from 'actions';
+import {configure} from 'configureStore';
+var store = configure();
+
+store.subscribe(() => {
+  console.log('New state', store.getState());
+});
+store.dispatch(actions.addTodo('Clean the yard'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
+
 // Load foundation
 $(document).foundation();
 
