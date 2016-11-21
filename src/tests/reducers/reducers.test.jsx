aa7395382a7dd49c8512420abc2a8490c096f032
Flavio Contreras <flaviohcontreras@live.com>
2016-11-21 00:22:44 -0200

    Add todos reducer and tests

--- src/tests/reducers/reducers.test.jsx
+++ src/tests/reducers/reducers.test.jsx
@@ -26,4 +26,35 @@ describe('Reducers', () => {
     });
   });
 
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var todoData = [{
+        id: 11,
+        text: 'Test features',
+        completed: false,
+        createdAt: 0,
+        completedAt: undefined
+      }];
+      var action = {
+        type: "TOGGLE_TODO",
+        id: 11
+      };
+      var res = reducers.todosReducer(df(todoData), df(action));
+
+      expect(res[0].completed).toBe(true);
+      expect(res[0].completedAt).toBeA('number');
+    });
+  });
+
 });
